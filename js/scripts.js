e420d170e92de7a6ab015985048cd3857abd55c5
Bradley Becker <beckerbradley11@yahoo.com>
2021-10-01 16:40:09 -0700

    task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-alert("Hello World");
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+let pokemonList = [{
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.5,
+        types: ['fire', 'dragon']
+    },
+    {
+        name: 'Pikachu',
+        height: 1.1,
+        types: ['electricity']
+    }
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1)
+    {
+         document.write(`${pokemonList[i].name} (${pokemonList[i].height}) Wow that's big <br>`)
+    }
+    else
+    {
+         document.write(`${pokemonList[i].name} (${pokemonList[i].height}) <br>`)
+    }
+}
