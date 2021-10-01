1c86a7eb4e4bbdecd6b5ed32a1c84c15e81c527f
Bradley Becker <beckerbradley11@yahoo.com>
2021-10-01 16:25:21 -0700

    changes

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/* alert("Hello World");
+let favoriteFood = "Pizza";
+document.write(favoriteFood); */
+
+let pokemonList = [{
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 4,
+        types: ['fire', 'dragon']
+    },
+    {
+        name: 'Pikachu',
+        height: 4,
+        types: ['electricity']
+    }
+];
+
+console.log(pokemonList)
