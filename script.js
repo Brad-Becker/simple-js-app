408e9628558781d20e60d47b786f145c3e4c835c
Bradley Becker <beckerbradley11@yahoo.com>
2021-10-18 19:22:45 -0700

    pokedex app

--- script.js
+++ script.js
@@ -1,65 +1,57 @@
 let pokemonRepository = (function () {
+  function pokemonListFromExternalSource() {
+    const apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let pokemonList = [];
+    fetch(apiURL)
+      .then(response => response.json())
+      .then(response => {
+        pokemonList = response.results;
+        pokemonList.forEach(pokemon => {
+          let newPokemonList = document.querySelector(".pokemon-list");
+          let listItemPokemon = document.createElement("li");
+          let button = document.createElement("button");
+          button.innerText = pokemon.name;
+          button.classList.add("button-class");
+          button.addEventListener('click', function () {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+              .then(response => response.json())
+              .then(response => {
+                console.log(response)
+                console.log(response.sprites.back_shiny)
+                let pokemonDetails = `Height: ${response.height}`
+                modal.showModal(`${pokemon.name}`, `${pokemonDetails}`, `${response.sprites.front_shiny}`);
+              })
+              .catch(err => console.error(err))
+          });
+          listItemPokemon.appendChild(button);
+          newPokemonList.appendChild(listItemPokemon);
+        })
+      })
+      .catch(error => console.log(error))
+  }
 
-  let pokemonList = [{
-      name: "Squirtle",
-      height: 0.5,
-      type: ["water", " dark", " normal"]
-    },
-    {
-      name: "Arbok",
-      height: 3.5,
-      type: ["poison", " fire", " ice"]
-    },
-    {
-      name: "Arcanine",
-      height: 1.9,
-      type: [" fire", " fairy", " steel"]
-    },
-    {
-      name: "Krabby",
-      height: 0.4,
-      type: [" water", " grass", " electric"]
-    }
-  ];
- 
   function getAll() {
     return pokemonList;
   }
-  
+
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
 
-  function addListItem(pokemon) {
-    let newPokemonList = document.querySelector(".pokemon-list");
-    let listItemPokemon = document.createElement("li");
-    let button = document.createElement("button");
-    button.innerText = pokemon.name;
-    button.classList.add("button-class");
-    button.addEventListener('click', function () {
-      let showDetails = `Name: ${pokemon.name}
-    Height: ${pokemon.height}
-    Type: ${[...pokemon.type]}`
-      console.log(showDetails);
-    });
-    listItemPokemon.appendChild(button);
-    newPokemonList.appendChild(listItemPokemon);
-  };
+  function main() {
+    pokemonRepository.pokemonListFromExternalSource()
+  }
 
+  //adding the return statement
   return {
     getAll: getAll,
     add: add,
-    addListItem: addListItem
+    main: main,
+    pokemonListFromExternalSource: pokemonListFromExternalSource
   };
 
 })();
 
-pokemonRepository.add({
-  name: "Charizard",
-  height: 4.2,
-  type: [" land", " poison", " magic"]
-});
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
-});
+
+pokemonRepository.main();
